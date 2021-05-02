41af7b3eecc32df131113857690d9094db7df678
wildmental <pbjworking@gmail.com>
2021-05-03 02:09:22 +0900

    READ 구현 2 - ORM Join 개선

--- codestates_school/__init__.py
+++ codestates_school/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from codestates_school.database import Base, db_session
 from flask import render_template
+from sqlalchemy.orm import joinedload, subqueryload
 
 
 def create_app(test_config=None):
@@ -45,17 +46,30 @@ def create_app(test_config=None):
 
     @app.route('/student', methods=['GET'], endpoint='student')
     def show_students():
-        students = Base.classes.student.query.all()
+        model = Base.classes.student
+        # joined load 는 left outer join 이 기본. (타겟 테이블 조회 시 쿼리 단일화 용도에 적절)
+        # inner join 이 필요한 경우 joined load 의 inner join 옵션 지정
+        students = db_session.query(model).options(joinedload(model.school),
+                                                   joinedload(model.department)).all()
         return render_template('students.html', students=students, urls=urls)
 
     @app.route('/lecture', methods=['GET'], endpoint='lecture')
     def show_lectures():
-        lectures = Base.classes.lecture.query.all()
+        model = Base.classes.lecture
+        subject = Base.classes.subject
+        room = Base.classes.room
+        # 2차 relation 참조 필요합 경우 joined load 체이닝으로 ORM 쿼리 통합
+        lectures = db_session.query(model).options(joinedload(model.subject).options(joinedload(subject.department)),
+                                                   joinedload(model.room).options(joinedload(room.building)),
+                                                   joinedload(model.professor)).all()
         return render_template('lectures.html', lectures=lectures, urls=urls)
 
     @app.route('/enroll', methods=['GET'], endpoint='enroll')
     def show_enrolls():
-        enrolls = Base.classes.enroll.query.all()
+        model = Base.classes.enroll
+        lecture = Base.classes.lecture
+        enrolls = db_session.query(model).options(joinedload(model.student),
+                                                  joinedload(model.lecture).options(joinedload(lecture.subject))).all()
         return render_template('enrolls.html', enrolls=enrolls, urls=urls)
 
     # 앱 종료시 DB connection 을 함께 종료
